33cc14e9de8ec281031d710906d2208c445731ff
erickruan1 <erick.silva1234543211@gmail.com>
2025-06-14 10:38:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ import cors from "cors"; // Importando a pkg CORS.
 const app = express();
 app.use(cors()); // Ativando o CORS package.
 const PORT = 3001; // Porta de acesso localhost.
+
 const names = { name: "abc" };
+const palavroes = {words: "poha;caralho"};
+const scan = {words: "giftcard steam"}
 const data = {
   number: 0,
-  number2: 0,
-  text: "OBMEP 03/06",
-  text2: "Olimpíada Brasileira de Matemática das Escolas Públicas.",
-  title2: "Prova de biologia 00/06",
-  desc2: "Prova de biologia sem data confirmada, estudos via teams utilizando os slides dos trabalhos.",
-  title3: "Not Found",
-  desc3: "Not found"
+  text: "",
+  text2: "",
 };
-const data2 = {words: "poxa;nossa"};
+
 const arrResponse = [
-  { name: "erickruan12", company: "abc" },
-  { name: "Alon", company: "Hashtag Treinamentos" },
+  { name: "erickruan12", company: "Piloty" },
+  { name: "erickruan12", company: "#Piloty V 1.0.3" },
 ];
 
 // Requisição padrão
@@ -34,8 +32,8 @@ app.get("/data", (req, res) => {
   res.json(data);
 });
 // Requisição de palavras como teste.
-app.get("/data2", (req, res) => {
-  res.send(data2);
+app.get("/palavroes", (req, res) => {
+  res.send(palavroes);
 });
 
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
